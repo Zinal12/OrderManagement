e902d886446bdb1d209a89056f3b156b1a42c99f
Zinal12 <111200203+Zinal12@users.noreply.github.com>
2022-10-05 22:22:44 -0400

    Connection database with VS Code (#6)

---
+++ OrderManagement_DataLayer/DataObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OrderManagement_DataLayer
+{
+    public class DataObject
+    {
+        
+            private String _ConnectionString { get; set; }
+            private SqlTransaction _transaction { get; set; }
+
+            public DataObject()
+            {
+                _ConnectionString = ConfigurationManager.ConnectionStrings["OrderCS"].ConnectionString;
+
+            }
+            public DataObject(SqlTransaction trans)
+            {
+                _ConnectionString = ConfigurationManager.ConnectionStrings["OrderCS"].ConnectionString;
+                _transaction = trans;
+            }
+
+            public string ConnectionString => _ConnectionString;
+            public SqlTransaction Transaction => _transaction;
+
+        
+    }
+}
